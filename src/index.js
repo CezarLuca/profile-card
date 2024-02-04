0d31a1ae880011ffe6dccbf7a900e484aa4bfd7c
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-02-04 15:45:08 +0100

    Card Challenge part 1

--- src/index.js
+++ src/index.js
@@ -5,14 +5,54 @@ import "./styles.css";
 function App() {
     return (
         <div className="card">
-            {/* <Avatar /> */}
+            <Avatar />
             <div className="data">
-                {/* <Intro /> */}
+                <Intro />
                 {/* Should contain one Skill component
         for each web dev skill that you have,
         customized with props */}
-                {/* <SkillList /> */}
+                <SkillList />
             </div>
+        </div>
+    );
+}
+
+function Avatar() {
+    return <img src="Avatar.png" alt="profile" className="profile" />;
+}
+
+function Intro() {
+    return (
+        <div>
+            <h1>Cezar Luca</h1>
+            <p>
+                Web Developer. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.
+            </p>
+        </div>
+    );
+}
+
+function SkillList() {
+    return (
+        <div>
+            <Skill name="HTML" />
+            <Skill name="CSS" />
+            <Skill name="JavaScript" />
+            <Skill name="React" />
+            <Skill name="Node.js" />
+            <Skill name="Express" />
+        </div>
+    );
+}
+
+function Skill(props) {
+    return (
+        <div className="skill">
+            <h3>{props.name}</h3>
         </div>
     );
 }
