2013b8e2ed13191f1951a15021cec5d38c4dcf62
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-02-04 16:25:08 +0100

    Added Styling and Emotes

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ function App() {
 }
 
 function Avatar() {
-    return <img src="Avatar.png" alt="profile" className="profile" />;
+    return <img src="Avatar.png" alt="profile" className="avatar" />;
 }
 
 function Intro() {
     return (
         <div>
             <h1>Cezar Luca</h1>
-            <p>
+            <p className="data">
                 Web Developer. Lorem ipsum dolor sit amet, consectetur
                 adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
@@ -38,21 +38,44 @@ function Intro() {
 
 function SkillList() {
     return (
-        <div>
-            <Skill name="HTML" />
-            <Skill name="CSS" />
-            <Skill name="JavaScript" />
-            <Skill name="React" />
-            <Skill name="Node.js" />
-            <Skill name="Express" />
+        <div className="skill-list">
+            <Skill name="HTML" bgColor="#ADD8E6" emote="strong" />
+            <Skill name="CSS" bgColor="#F0E68C" emote="strong" />
+            <Skill name="JavaScript" bgColor="#90EE90" emote="strong" />
+            <Skill name="React" bgColor="#FFA07A" emote="good" />
+            <Skill name="Node.js" bgColor="#ADD8E6" emote="stong" />
+            <Skill name="Express" bgColor="red" emote="baby" />
         </div>
     );
 }
 
 function Skill(props) {
+    const emote = props.emote;
+    const bgColor = props.bgColor;
+    const style = {
+        backgroundColor: bgColor,
+    };
+
+    let emoji;
+    switch (emote) {
+        case "strong":
+            emoji = "💪";
+            break;
+        case "good":
+            emoji = "👍";
+            break;
+        case "baby":
+            emoji = "👶";
+            break;
+        default:
+            emoji = "";
+    }
+
     return (
-        <div className="skill">
-            <h3>{props.name}</h3>
+        <div className="skill" style={style}>
+            <h3>
+                {props.name} {emoji}
+            </h3>
         </div>
     );
 }
